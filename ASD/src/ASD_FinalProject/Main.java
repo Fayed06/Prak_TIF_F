f27224e6304c69dccf5f3877e744d08041bbe5fc
XFAYZ\ASUS <alfayed06@student.ub.ac.id>
2021-05-19 18:54:01 +0800

    adding some comment for the description

--- ASD/src/ASD_FinalProject/Main.java
+++ ASD/src/ASD_FinalProject/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Queue q = new Queue();
         int jmlh = 1;
-
+//        path file nya tergantung pemilihan lokasi "file.txt"
         try (Scanner fileReader = new Scanner(Paths.get("ASD/file.txt"))) {
             while (fileReader.hasNextLine()) {
                 String line = fileReader.nextLine();

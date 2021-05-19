811113e56cb3fdd7eea9bd57fae2e899296b998f
XFAYZ\ASUS <alfayed06@student.ub.ac.id>
2021-05-19 18:37:17 +0800

    making jumlah layanan read from file

--- ASD/src/ASD_FinalProject/Main.java
+++ ASD/src/ASD_FinalProject/Main.java
@@ -6,40 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
         Queue q = new Queue();
-
-//        Node p1 = new Node(10, 5);
-//        Node p2 = new Node(20, 6);
-//        Node p3 = new Node(21, 5);
-//        Node p4 = new Node(22, 4);
-//        Node p5 = new Node(25, 3);
-//        Node p6 = new Node(26, 6);
-//        Node p7 = new Node(30, 5);
-//        Node p8 = new Node(36, 4);
-//        Node p9 = new Node(48, 5);
-//
-//        q.addLast(p1);
-//        q.addLast(p2);
-//        q.addLast(p3);
-//        q.addLast(p4);
-//        q.addLast(p5);
-//        q.addLast(p6);
-//        q.addLast(p7);
-//        q.addLast(p8);
-//        q.addLast(p9);
-//        q.FCFS();
+        int jmlh = 1;
 
         try (Scanner fileReader = new Scanner(Paths.get("ASD/file.txt"))) {
             while (fileReader.hasNextLine()) {
                 String line = fileReader.nextLine();
-                String[] parts = line.split(",");
+                if(!line.contains(",")) {
+                    jmlh = Integer.valueOf(line);
+                } else {
+                    String[] parts = line.split(",");
 
-                Node node = new Node(Integer.valueOf(parts[0]), Integer.valueOf(parts[1]));
-                q.addLast(node);
+                    Node node = new Node(Integer.valueOf(parts[0]),
+                            Integer.valueOf(parts[1]));
+                    q.addLast(node);
+                }
             }
         } catch (Exception e) {
             System.out.println("Reading the file failed.");
         }
 
-        q.FCFS();
+        q.FCFS(jmlh);
     }
 }
--- ASD/src/ASD_FinalProject/Queue.java
+++ ASD/src/ASD_FinalProject/Queue.java
@@ -1,21 +1,21 @@
 package ASD_FinalProject;
-import java.util.Scanner;
+import java.util.*;
 
 class Queue {
     int size = 0;
     Node head;
     Node tail;
 
-    boolean isEmpty(){
+    boolean isEmpty() {
         return size == 0;
     }
 
-    int size(){
+    int size() {
         return this.size;
     }
 
-    void addFirst(Node temp){
-        if(isEmpty()){
+    void addFirst(Node temp) {
+        if (isEmpty()) {
             head = tail = temp;
             size++;
             return;
@@ -25,8 +25,8 @@ class Queue {
         size++;
     }
 
-    void addLast(Node temp){
-        if(isEmpty()){
+    void addLast(Node temp) {
+        if (isEmpty()) {
             head = tail = temp;
             size++;
             return;
@@ -36,13 +36,13 @@ class Queue {
         size++;
     }
 
-    void removeFirst(){
-        if(isEmpty()){
+    void removeFirst() {
+        if (isEmpty()) {
             System.out.println("not found");
         }
-        if(size == 1){
+        if (size == 1) {
             head = tail = null;
-        }else {
+        } else {
             Node temp = head;
             temp = temp.next;
             head = temp;
@@ -51,11 +51,11 @@ class Queue {
         size--;
     }
 
-    void removeLast(){
-        if(isEmpty()){
+    void removeLast() {
+        if (isEmpty()) {
             System.out.println("not found");
         }
-        if(size == 1){
+        if (size == 1) {
             head = tail = null;
         } else {
             Node temp = head;
@@ -68,32 +68,33 @@ class Queue {
         size--;
     }
 
-    void print(){
-        if(isEmpty()){
+    void print() {
+        if (isEmpty()) {
             System.out.println("kosong");
             return;
         }
         Node temp = head;
-        for(int i = 0; i < size; i++){
-            System.out.print("Pelanggan " + (i+1) + ": " + temp.arrival + ", " + temp.bursttime + "\n");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Pelanggan " + (i + 1) + ": " +
+                    temp.arrival + ", " + temp.bursttime + "\n");
             temp = temp.next;
         }
     }
 
-    void sortData(){
+    void sortData() {
         Node current = head;
         Node index = null;
         int arrivaltemp;
         int bursttemp;
         String namatemp;
 
-        if (head == null){
+        if (head == null) {
             return;
         } else {
-            while(current != null){
+            while (current != null) {
                 index = current.next;
-                while(index != null){
-                    if(current.arrival > index.arrival){
+                while (index != null) {
+                    if (current.arrival > index.arrival) {
                         arrivaltemp = current.arrival;
                         bursttemp = current.bursttime;
                         namatemp = current.nama;
@@ -111,19 +112,18 @@ class Queue {
         }
     }
 
-    void FCFS(){
-        Scanner in = new Scanner(System.in);
-        if(isEmpty()){
+    void FCFS(int jmlhLayanan) {
+        if (isEmpty()) {
             System.out.println("kosong");
             return;
         }
         Node temp = head;
 
-        System.out.print("Jumlah layanan:");
-        int queues = in.nextInt();
+        System.out.print("Jumlah layanan: " + jmlhLayanan);
+        int queues = jmlhLayanan;
 
         Queue[] queue = new Queue[queues];
-        int[][] wtime= new int[queues][size()];
+        int[][] wtime = new int[queues][size()];
         for (int i = 0; i < queues; i++) {
             queue[i] = new Queue();
         }
@@ -138,23 +138,25 @@ class Queue {
         double totalWT = 0;
         double rt = 0;
 
-        System.out.println("" +
-                "============================================================================================\n" +
-                "| Pelanggan | Datang | Waktu Layanan |   	Queue 	   | Mulai  | Selesai | Waktu Tunggu |");
-        while (count < size()){
+        System.out.println(""
+                + "\n============================================================================================\n"
+                + "| Pelanggan | Datang | Waktu Layanan |   	Queue 	   | Mulai  | Selesai | Waktu Tunggu |"
+                + "");
+        while (count < size()) {
             int[] times = new int[queues];
 
-            if(wtime[qtype][0] != 0 || count == 0) {
+            if (wtime[qtype][0] != 0 || count == 0) {
                 wtime[qtype][0] = 0;
-            }else{
+            } else {
                 int waitPrev = 0;
-                for (int i = size()-1; i > 0; i--) {
-                    if (wtime[qtype][i]!=0) {
+                for (int i = size() - 1; i > 0; i--) {
+                    if (wtime[qtype][i] != 0) {
                         waitPrev = wtime[qtype][i];
                         break;
                     }
                 }
-                wtime[qtype][count] = aPrev[qtype] + bPrev[qtype] - temp.arrival + waitPrev;
+                wtime[qtype][count] = aPrev[qtype] + bPrev[qtype] -
+                        temp.arrival + waitPrev;
                 if (wtime[qtype][count] < 0) {
                     wtime[qtype][count] = 0;
                 }
@@ -163,52 +165,55 @@ class Queue {
             mulai[qtype] = temp.arrival + wtime[qtype][count];
             selesai[qtype] = mulai[qtype] + temp.bursttime;
 
-            queue[qtype].addLast(new Node((count + 1) + "", selesai[qtype]));
-            while(queue[qtype].head.selesai < temp.arrival) {
+            queue[qtype].addLast(new Node((count + 1) + "",
+                    selesai[qtype]));
+            while (queue[qtype].head.selesai < temp.arrival) {
                 queue[qtype].removeFirst();
             }
 
-            System.out.printf("%8S %8S %12S %22S %10S %7S %11S %n", temp.nama + " Q" + (qtype+1), temp.arrival, temp.bursttime, "Q" + (qtype+1) + ": " + queue[qtype].show(), mulai[qtype], selesai[qtype], wtime[qtype][count]);
+            System.out.printf("%8S %8S %12S %22S %10S %7S %11S %n",
+                    temp.nama + " Q" + (qtype + 1), temp.arrival,
+                    temp.bursttime, "Q" + (qtype + 1) + ": " +
+                            queue[qtype].show(), mulai[qtype], selesai[qtype],
+                    wtime[qtype][count]);
 
-            //ngitung rata-rata
+            // ngitung rata-rata
             totalWT += wtime[qtype][count];
-
-
             bPrev[qtype] = temp.bursttime;
             aPrev[qtype] = temp.arrival;
             temp = temp.next;
-
             count++;
 
-            if(count != 0 && count < size()){
-                //jika masuk queue pertama kali
+            if (count != 0 && count < size()) {
+                // jika masuk queue pertama kali
                 for (int i = 0; i < queues; i++) {
                     if (wtime[i][0] != 0) {
                         qtype = i;
                     }
                 }
-                //menghitung waiting time setiap queue
+                // menghitung waiting time setiap queue
                 for (int i = 0; i < queues; i++) {
                     times[i] = selesai[i] - temp.arrival;
-                    //if(times[i] < 0) times[i] = 0;
+                    // if(times[i] < 0) times[i] = 0;
                 }
-                //membandingkan total waiting time setiap queue
+                // membandingkan total waiting time setiap queue
                 int temp2 = times[0];
                 int change = 0;
-                for (int i = 0; i < queues-1; i++) {
-                    if (temp2<times[i+1]){
+                for (int i = 0; i < queues - 1; i++) {
+                    if (temp2 < times[i + 1]) {
                         qtype = change;
                     } else {
                         change = i + 1;
                         qtype = change;
-                        temp2 = times[i+1];
+                        temp2 = times[i + 1];
                     }
                 }
             }
         }
-        System.out.println("============================================================================================");
+        System.out.println(
+                "============================================================================================");
 
-        rt = totalWT /this.size();
+        rt = totalWT / this.size();
 
         System.out.printf("%-80s %f %n", "Rata-Rata Waktu Tunggu", rt);
     }
